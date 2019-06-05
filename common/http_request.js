41684c5186196e72ec867cd1e13bcc181c0781ab
imbacc <873243031@qq.com>
2019-06-05 14:34:21 +0800

    Update http_request.js

--- common/http_request.js
+++ common/http_request.js
@@ -2,24 +2,23 @@
  * 拦截请求
  */
 import http from './vmeitime-http/interface.js'
+import is_fun_tools from './is_fun_tools'
 
-const error_msg = (err) => {
-	uni.showToast({
-		title: err || '服务器打瞌睡了...',
-		icon: 'none'
+const call_fun_vuex_error_msg = (msg) => {
+	is_fun_tools.check_login().then((res)=>{
+		!res ? is_fun_tools.to_msg('请登录授权...') : (msg ? is_fun_tools.to_msg(msg) : is_fun_tools.to_msg()) 
 	})
 }
 
 //设置请求拦截
 http.interceptor.request = (config) => {
 	
-	let token = uni.getStorageSync("token") | ""
-	
-	console.log("token="+token)
+	let token = uni.getStorageSync("token")
+	token instanceof String ? token = JSON.parse(token) : console.log("token="+token)
 	
 	//添加通用参数 Token
 	config.header = {
-	    "Authorization": `Bearer ${token}`
+		Authorization: `Bearer ${token}`
 	}
 	
 	// config.requestId = new Date().getTime()
@@ -29,11 +28,8 @@ http.interceptor.request = (config) => {
 //设置响应拦截
 http.interceptor.response = (res) => {
 	
-	if(res.errMsg.toString().indexOf("fail") != -1 || res.statusCode == 0){ 
-		uni.showToast({  
-			title: "服务器打瞌睡了...",
-			icon:'none'
-		})
+	if(res.errMsg.toString().indexOf("fail") != -1 || res.statusCode == 0){
+		call_fun_vuex_error_msg()
 		return false
 	}
 	
@@ -43,8 +39,18 @@ http.interceptor.response = (res) => {
 	if(res.data.hasOwnProperty("result")){
 		 return res.data
 	}else{
-		console.log("res.data.error.message="+res.data.error.message)
-		error_msg(res.data.error.message)
+		console.log('---------------------------------------------')
+		console.log(res)
+		console.log('---------------------------------------------')
+		if(res.data.error == "invalid_token"){
+			call_fun_vuex_error_msg('服务器',res)
+		}else{
+			if(res.data.error.hasOwnProperty("message")){
+				return res
+			} 
+			if(res.statusCode != 200) call_fun_vuex_error_msg()
+		}
+		console.log("res.data.error="+res.data.error)
 		return false
 	}
 	
