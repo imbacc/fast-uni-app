75c01fc7cfa690ed47f51762b6a3eea0d5d6065f
imbacc <imbacc@qq.com>
2023-03-31 14:23:03 +0800

    update: mitt export

--- src/tools/mitt.ts
+++ src/tools/mitt.ts
@@ -1,18 +1,23 @@
+export const uniEmit = (key: string, val: any) => {
+  uni.$emit(key, val)
+}
+
+export const uniOn = (key: string, fun: (result: any) => void) => {
+  uni.$off(key, fun)
+  uni.$on(key, fun)
+}
+
+export const uniOff = (key: string, fun: (result: any) => void) => {
+  uni.$off(key, fun)
+}
+
+export const uniOnce = (key: string, fun: (result: any) => void) => {
+  uni.$once(key, fun)
+}
+
 export default {
-  uniEmit(key: string, val: any) {
-    uni.$emit(key, val)
-  },
-
-  uniOn(key: string, fun: (result: any) => void) {
-    uni.$off(key, fun)
-    uni.$on(key, fun)
-  },
-
-  uniOff(key: string, fun: (result: any) => void) {
-    uni.$off(key, fun)
-  },
-
-  uniOnce(key: string, fun: (result: any) => void) {
-    uni.$once(key, fun)
-  },
+  uniEmit,
+  uniOn,
+  uniOff,
+  uniOnce,
 }
